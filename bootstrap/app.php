97b506163f5e8f8e3388844069ef95765a0b0be0
unknown <heyreza8@gmail.com>
2025-05-03 20:52:14 +0330

    set middleware for dashboard and auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn() => route('auth.login.form'));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Dashboard\GatewayController;
 use App\Http\Controllers\Dashboard\PaymentController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->name('auth.')->group(function(){
+Route::prefix('auth')->name('auth.')->middleware('guest')->group(function(){
     Route::get('register',[RegisterController::class,'form'])->name('register.form');
     Route::post('register',[RegisterController::class,'register'])->name('register');
     Route::get('verify/{user:email}',[RegisterController::class,'verify'])->name('register.verify');
@@ -15,7 +15,7 @@ Route::prefix('auth')->name('auth.')->group(function(){
     Route::post('login',[LoginController::class,'login'])->name('login');
 });
 
-Route::prefix('dashboard')->name('dashboard.')->group(function(){
+Route::prefix('dashboard')->name('dashboard.')->middleware('auth')->group(function(){
     Route::view('/','dashboard.index')->name('index');
 
     Route::resource('gateway',GatewayController::class);
