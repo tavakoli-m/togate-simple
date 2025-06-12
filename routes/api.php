4368e0e8b8ffcd0591f4842133e749fb51ad0530
unknown <heyreza8@gmail.com>
2025-06-12 15:36:07 +0330

    refactor api.json

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,9 @@
 
 use App\Http\Controllers\Api\PaymentFormController;
 use App\Http\Controllers\Api\RequestPaymentController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('request.json',RequestPaymentController::class);
-Route::get('start.json/{payment:token}',[PaymentFormController::class,'start']);
-Route::get('check.json/{payment:token}',[PaymentFormController::class,'check']);
-Route::get('status.json/{payment:token}',[PaymentFormController::class,'status']);
+Route::post('request.json', RequestPaymentController::class);
+Route::get('start.json/{payment:token}', [PaymentFormController::class, 'start']);
+Route::get('check.json/{payment:token}', [PaymentFormController::class, 'check']);
+Route::get('status.json/{payment:token}', [PaymentFormController::class, 'status']);
