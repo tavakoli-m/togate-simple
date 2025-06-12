c376b3bf696259dbcc9614041008ad12bc185f74
unknown <heyreza8@gmail.com>
2025-06-12 14:49:46 +0330

    update register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -57,8 +57,8 @@
 
                 <div>
                     <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">ثبت
-                        نام
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        ارسال لینک فعال سازی
                     </button>
                 </div>
             </form>
