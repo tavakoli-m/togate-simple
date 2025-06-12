f7b5089517816f78dc949745b1b303d07d0d83eb
unknown <heyreza8@gmail.com>
2025-06-12 19:45:17 +0330

    update payment config

--- config/payment.php
+++ config/payment.php
@@ -2,7 +2,7 @@
 
 return [
     // Percent of Payment (0.5 => 50%)
-    'fee' => 0.5,
+    'fee' => 0.2,
 
     // Address To Sent Fee
     'site_tron_wallet' => 'TJUcSwY37FyY3vB1UkSKcsbkZytTUh3Yx8'
